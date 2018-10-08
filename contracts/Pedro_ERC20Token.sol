0e2c4510b245b3c8de7d01a4ba6c486e87183936
carlosescom <carlos.escom.ipn@gmail.com>
2018-10-08 15:51:58 -0500

    :heavy_plus_sign: Added our brand new general structure for Pedro Token.

---
+++ contracts/Pedro_ERC20Token.sol
@@ -0,0 +1,15 @@
+pragma solidity ^0.4.23;
+
+import "openzeppelin-solidity/contracts/token/ERC20/StandardToken.sol";
+
+contract TutorialToken is StandardToken {
+  string public name = "Pedro Token";
+  string public symbol = "PEDRO";
+  uint public decimals = 2;
+  uint public INITIAL_SUPPLY = 255000000 * 10**uint(decimals);
+
+  constructor() public {
+    totalSupply_ = INITIAL_SUPPLY;
+    balances[msg.sender] = INITIAL_SUPPLY;
+  }
+}
