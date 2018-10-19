3e423c34a3d8b7a16e83ca9c9b3d17a4483fdbc2
carlosescom <carlos.escom.ipn@gmail.com>
2018-10-19 04:09:36 -0500

    :bug: Specifed the address to deploy from.

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,5 +1,10 @@
 var Pedro_ERC20Token = artifacts.require("Pedro_ERC20Token");
 
 module.exports = function(deployer) {
-  deployer.deploy(Pedro_ERC20Token);
+  deployer.deploy(
+    Pedro_ERC20Token,
+    {
+      from:"0xa32ac2c1646ead762a44f1e14c3093273e118d47"
+    }
+  );
 };
