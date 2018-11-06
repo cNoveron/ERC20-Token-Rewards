828ffab646d5b1338e1a77fe58885f040d548cbf
carlosescom <carlos.escom.ipn@gmail.com>
2018-11-05 22:12:30 -0600

    :sparkles: Now Balance components are colored upon account addresses.

--- src/layouts/components/Balance.js
+++ src/layouts/components/Balance.js
@@ -2,20 +2,35 @@ import React, { Component } from 'react'
 import { ContractData } from 'drizzle-react-components'
 
 class Balance extends Component {
-
   render() {
-    return <p>
-      <strong>balanceOf : {this.props.addressStr}</strong> 
-      <ContractData 
-        contract="Pedro_ERC20Token"
-        method="balanceOf" 
-        methodArgs={[this.props.addressStr]} 
-      />{" "}
-      <ContractData 
-        contract="Pedro_ERC20Token"
-        method="symbol"
-      />
-    </p>
+    let addressStyle = {
+      color: "#"+this.props.addressStr.substring(2,8),
+      display: 'inline',
+    }
+    let balanceStyle = {
+      color: 'white',
+      fontWeight: 'bold',
+      backgroundColor: addressStyle.color,
+      padding: '3px 7px',
+      borderRadius: '4px' 
+    }
+    return <div>      
+      balanceOf:{' '}
+      <strong style={addressStyle}>
+        {this.props.addressStr}
+      </strong>{' '}
+      <div style={balanceStyle}>
+        <ContractData 
+          contract="Pedro_ERC20Token"
+          method="balanceOf" 
+          methodArgs={[this.props.addressStr]} 
+        />{' '}
+        <ContractData
+          contract="Pedro_ERC20Token"
+          method="symbol"
+        />
+      </div>
+    </div>
   }
 }
 
