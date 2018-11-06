0221a635983c934cae16be6d0c0aeaa4264cba03
carlosescom <carlos.escom.ipn@gmail.com>
2018-11-05 20:03:27 -0600

    Handle which account to deployed from, depending on current network.

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,10 +1,13 @@
 var Pedro_ERC20Token = artifacts.require("Pedro_ERC20Token");
 
-module.exports = function(deployer) {
-  deployer.deploy(
-    Pedro_ERC20Token,
-    {
-      from:"0xa32ac2c1646ead762a44f1e14c3093273e118d47"
-    }
-  );
+module.exports = function(deployer, network, accounts) {
+  var sendOptions = {
+    from: accounts[0]
+   }
+   if (network == "ropsten") {
+     sendOptions.from = "0xA32ac2C1646Ead762A44f1e14c3093273E118D47".toLowerCase()
+   } /* else {
+     // Perform a different step otherwise.
+   } */
+   deployer.deploy(Pedro_ERC20Token,sendOptions);
 };
