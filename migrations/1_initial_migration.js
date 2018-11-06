8dc6c22f02ec7d43ab40934a6bece5740edfba96
carlosescom <carlosescom>
2018-11-05 20:03:27 -0600

    Handle which account to deployed from, depending on current network.

--- migrations/1_initial_migration.js
+++ migrations/1_initial_migration.js
@@ -1,10 +1,13 @@
 var Migrations = artifacts.require("Migrations");
 
-module.exports = function(deployer) {
-  deployer.deploy(
-    Migrations,
-    {
-      from:"0xA32ac2C1646Ead762A44f1e14c3093273E118D47".toLowerCase()
-    }
-  );
+module.exports = function(deployer, network, accounts) {
+  var sendOptions = {
+   from: accounts[0]
+  }
+  if (network == "ropsten") {
+    sendOptions.from = "0xA32ac2C1646Ead762A44f1e14c3093273E118D47".toLowerCase()
+  } /* else {
+    // Perform a different step otherwise.
+  } */
+  deployer.deploy(Migrations,sendOptions);
 };
