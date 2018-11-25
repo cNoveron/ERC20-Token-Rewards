c005ff0e3d92cce16a7585fe293d51659347a30b
carlosescom <carlosescom>
2018-11-24 23:43:58 -0600

    Included ServiceStateController for deployment.

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,13 +1,15 @@
 var Pedro_ERC20Token = artifacts.require("Pedro_ERC20Token");
+var ServiceStateController = artifacts.require("ServiceStateController");
 
 module.exports = function(deployer, network, accounts) {
-  var sendOptions = {
-    from: accounts[0]
-   }
-   if (network == "ropsten") {
-     sendOptions.from = "0xA32ac2C1646Ead762A44f1e14c3093273E118D47".toLowerCase()
-   } /* else {
-     // Perform a different step otherwise.
-   } */
-   deployer.deploy(Pedro_ERC20Token,sendOptions);
+    var sendOptions = {
+        from: accounts[2]
+    }
+    if (network == "ropsten") {
+        sendOptions.from = "0xA32ac2C1646Ead762A44f1e14c3093273E118D47".toLowerCase()
+    } /* else {
+        // Perform a different step otherwise.
+    } */
+    //deployer.deploy(Pedro_ERC20Token,sendOptions);
+    deployer.deploy(ServiceStateController,sendOptions);
 };
