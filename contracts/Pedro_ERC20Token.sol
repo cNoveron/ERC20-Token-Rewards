7263df93272325a9c698ea951de31f7e8ba11996
carlosescom <carlos.escom.ipn@gmail.com>
2018-12-13 18:00:18 -0600

    :lock: Implementing reentrancy-resistent transfer feature.

--- contracts/Pedro_ERC20Token.sol
+++ contracts/Pedro_ERC20Token.sol
@@ -57,17 +57,45 @@ contract Pedro_ERC20Token {
     * @param _to The address to transfer to.
     * @param _value The amount to be transferred.
     */
-    function transfer(address _to, uint256 _value) public returns (bool) {
+    function transfer(
+        address _to, 
+        uint256 _value
+    ) 
+    public 
+    returns (bool) 
+    {
+        _transfer(_to, _value);
+    }
+    
+    function _transfer(
+        address _to, 
+        uint256 _value
+    ) 
+    internal
+    nonReentrant
+    returns (bool) 
+    {
         require(_to != address(0));
         require(_value <= balances[msg.sender]);
 
         balances[msg.sender] = balances[msg.sender].sub(_value);
         balances[_to] = balances[_to].add(_value);
         emit Transfer(msg.sender, _to, _value);
+        emit Transfer(msg.sender, msg.sender, _to, _value);
         return true;
     }
     
-    event Transfer(address indexed from, address indexed to, uint256 value);
+    event Transfer(
+        address indexed _from, 
+        address indexed _to, 
+        uint256 value
+    );
+    event Transfer(
+        address indexed _spender,
+        address indexed _from,
+        address indexed _to,
+        uint256 _value
+    );
 
     /**
     * @dev Gets the balance of the specified address.
