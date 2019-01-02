c2c98f46ff27db1ab7a030278df77cc36ebe0e47
carlosescom <carlosescom>
2019-01-01 23:11:18 -0600

    Migrated to a better state machine description format.

--- contracts/IServiceStateController.sol
+++ contracts/IServiceStateController.sol
@@ -46,85 +46,50 @@ interface IServiceStateController {
         address     requesterAddress
     );
 
-    /**
-    * @dev Only a service provider should use this to offer his services in favor
-    * of a previously identified serviceRequest.
-    * @param reviewId       uint32      The identifier of the review due to be submitted.
-    * @param offerTimestamp uint64      The UNIX time when the offerServices method was called.
-    * @param price          uint16      The provider's price in USD.
-    */
+
+
+
+    /// @notice Only a service provider should use this to offer his services in favor.
+    /// @param reviewId       uint32      The identifier of the review due to be submitted.
+    /// @param offerTimestamp uint64      The UNIX time when offerServices was called.
+    /// @param price          uint16      The provider's price in USD.
+    
     function offerServices(uint32 reviewId, uint64 offerTimestamp, uint16 price) 
     external 
     returns(uint8);
 
 
-    /// @dev 403 Forbidden - A 1st call for requestServices, 
-    /// then A 2nd call for offerServices
-    /// from *THE_SAME address as that of the call for requestServices.
+    /// @dev 403 Forbidden 
     ///
-    /// This is implemented through this modifier in ReviewsController.sol 
-    /// reviewId_isInState(state.AWATING_DEAL, reviewId)
+    /// requestServices (ANY, ANY, ANY,  { from: adr0 }) @return 1
+    /// offerServices   (ANY, ANY, ANY,  { from: adr0 }) @return Failed: Revert();
     ///
+    /// requestServices (  0, 0,   ANY,  { from: adr0 }) @return 1
+    /// offerServices   (  0, 0,   ANY,  { from: adr1 }) @return Failed: Revert();
     ///
-    /// This batch of tests will be a fist call with the following parameters:
-    /// [ R-0000 ] requestServices(0, 0, [1,2,3],   { from: 0xa })  @return 1
+    /// requestServices (  0, 0,   ANY,  { from: adr0 }) @return 1
+    /// offerServices   (  1, 0,   ANY,  { from: adr0 }) @return Failed: Revert();
     ///
-    /// Followed by each of the following calls as second call, in each individual test:
-    /// [ O-0000 ] offerServices(  0, 0, 100,       { from: 0xa })  <--  SHALL NOT PASS
-    /// [ O-0010 ] offerServices(  0, 0, 2222,      { from: 0xa })  <--  SHALL NOT PASS
-    /// [ O-0100 ] offerServices(  0, 1, 100,       { from: 0xa })  <--  SHALL NOT PASS
-    /// [ O-0110 ] offerServices(  0, 1, 2222,      { from: 0xa })  <--  SHALL NOT PASS
-    /// [ O-1000 ] offerServices(  1, 0, 100,       { from: 0xa })  <--  SHALL NOT PASS
-    /// [ O-1010 ] offerServices(  1, 0, 2222,      { from: 0xa })  <--  SHALL NOT PASS
-    /// [ O-1100 ] offerServices(  1, 1, 100,       { from: 0xa })  <--  SHALL NOT PASS
-    /// [ O-1110 ] offerServices(  1, 1, 2222,      { from: 0xa })  <--  SHALL NOT PASS
+    /// requestServices (  0, 0,   ANY,  { from: adr0 }) @return 1
+    /// offerServices   (  1, 1,   ANY,  { from: adr1 }) @return Failed: Revert();
     ///
-    /// Golden rule:
-    /// [ R-0000 ] requestServices(0, 0, [1,2,3],   { from: 0xa })  @return 1
-    /// [ O-XXX0 ] requestServices(*ANY, *ANY, *ANY,{ from: 0xa })  <--  SHALL NOT PASS
+    /// requestServices (  0, 0,   ANY,  { from: adr0 }) @return 1
+    /// offerServices   (  0, 1,   ANY,  { from: adr1 }) @return 1
+    /// offerServices   (  0, 1,   ANY,  { from: adr1 }) @return Failed: Revert();
+    ///
+    /// requestServices (  0, 0,   ANY,  { from: adr0 }) @return 1
+    /// offerServices   (  0, 1,   ANY,  { from: adr1 }) @return 1
+    /// offerServices   (  0, 2,   ANY,  { from: adr1 }) @return Failed: Revert();
 
 
-    /// @dev 403 Forbidden - A 1st call for requestServices, 
-    /// then a 2nd call for offerServices
-    /// for *THE_SAME given reviewId,
-    /// at *THE_SAME timestamp as that reviewId,
-    /// from a *DIFFERENT address than that of the first call.
+    /// @dev 200 OK - A 1st call for requestServices.
     ///
-    /// Theoretically, since request and offer timestamps can be retrieved and compared by both
-    /// web server and blockchain server. The following call sequences to ReviewsController.sol  
-    /// should not be made from the blockchain server. Nonetheless, security measures MUST be 
-    /// implemented within the contract for users who choose to operate their own wallets.
+    /// requestServices (  0, 0,   ANY,  { from: adr0 }) @return 1
+    /// offerServices   (  0, 1,   ANY,  { from: adr1 }) @return 1
     ///
-    ///
-    /// This batch of tests will be a fist call with the following parameters:
-    /// [ R-0000 ] requestServices(0, 0, [1,2,3],   { from: 0xa })  @return 1
-    ///
-    /// Followed by each of the following calls as second call, in each individual test:
-    /// [ O-0001 ] offerServices(  0, 0, 100,       { from: 0xb })  <--  SHALL NOT PASS
-    /// [ O-0011 ] offerServices(  0, 0, 2222,      { from: 0xb })  <--  SHALL NOT PASS
-    ///
-    /// Golden rule:
-    /// [ R-0000 ] requestServices(0, 0, [1,2,3],   { from: 0xa })  @return 1
-    /// [ O-00X1 ] requestServices(1, 0, *ANY,      { from: *ANY }) <--  SHALL NOT PASS
-
-
-    /// @dev 200 OK - A 1st call for requestServices, 
-    /// then a 2nd call for offerServices
-    /// for *THE_SAME reviewId as that of the 1st call,
-    /// at a *DIFFERENT timestamp than that of the 1st call,
-    /// from a *DIFFERENT address than that of the 1st call.
-    ///
-    ///
-    /// This batch of tests will be a fist call with the following parameters:
-    /// [ R-0000 ] requestServices(0, 0, [1,2,3],   { from: 0xa })  @return 1
-    ///
-    /// Followed by each of the following calls as second call, in each individual test:
-    /// [ O-0101 ] offerServices(  0, 1, 100,       { from: 0xb })  @return 1
-    /// [ O-0111 ] offerServices(  0, 1, 2222,      { from: 0xb })  @return 1
-    ///
-    /// Golden rule:
-    /// [ R-0000 ] requestServices(0, 0, [1,2,3],   { from: 0xa })  @return 1
-    /// [ O-01X1 ] offerServices(  0, 1, 2222,      { from: 0xb })  @return 1
+    /// requestServices (  0, 0,   ANY,  { from: adr0 }) @return 1
+    /// offerServices   (  0, 1,   ANY,  { from: adr1 }) @return 1
+    /// offerServices   (  1, 2,   ANY,  { from: adrN }) @return 1
 
 
     /// @dev 403 Forbidden - A 1st call for requestServices, 
