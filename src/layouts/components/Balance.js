0067c61c3e746852c163eae49475e98230405268
carlosescom <carlosescom>
2019-01-04 04:01:19 -0600

    :space_invader: Just for fun

--- src/layouts/components/Balance.js
+++ src/layouts/components/Balance.js
@@ -2,39 +2,76 @@ import React, { Component } from 'react'
 import { ContractData } from 'drizzle-react-components'
 
 class Balance extends Component {
+  constructor(props) {
+    super(props)
+    this.colorStrings = {
+      primary: props.currentAccount.substring(2, 8),
+      secondary: props.currentAccount.substring(9, 15)
+    }
+    this.state = {
+      address: {
+        color: '#' + this.colorStrings.primary,
+        display: 'inline'
+      },
+      balance : {
+        color: 'white',
+        fontWeight: 'bold',
+        backgroundColor: '#' + this.colorStrings.secondary,
+        padding: '3px 7px',
+        borderRadius: '4px'
+      }
+    }
+  }
+
+  getColors(props) {
+    this.colorStrings = {
+      primary: props.currentAccount.substring(2, 8),
+      secondary: props.currentAccount.substring(9, 15)
+    }
+  }
+
+  componentDidUpdate(prevProps) {
+    if (this.props !== prevProps) {
+      this.getColors(this.props)
+      this.setState({
+        address: {
+          color: '#' + this.colorStrings.primary,
+          display: 'inline'
+        },
+        balance: {
+          color: 'white',
+          fontWeight: 'bold',
+          backgroundColor: '#' + this.colorStrings.secondary,
+          padding: '3px 7px',
+          borderRadius: '4px'
+        }
+      })
+    }
+  }  
+
   render() {
-    let addressStyle = {
-      color: "#"+this.props.addressStr.substring(2,8),
-      display: 'inline',
-    }
-    let balanceStyle = {
-      color: 'white',
-      fontWeight: 'bold',
-      backgroundColor: addressStyle.color,
-      padding: '3px 7px',
-      borderRadius: '4px' 
-    }
-    return <div>      
-      balanceOf:{' '}
+    return(
+      <div>      
+        balanceOf:{' '}
         <strong style={this.state.address}>
           {this.props.currentAccount}
-      </strong>{' '}
+        </strong>{' '}
         <div style={this.state.balance}>
-        <ContractData 
+          <ContractData
             drizzle={this.props.drizzle}
             drizzleState={this.props.drizzle.store.getState()}
-          contract="Pedro_ERC20Token"
-          method="balanceOf" 
+            contract="Pedro_ERC20Token"
+            method="balanceOf" 
             methodArgs={[this.props.currentAccount]} 
-        />{' '}
-        <ContractData
+          />{' '}
+          <ContractData
             drizzle={this.props.drizzle}
             drizzleState={this.props.drizzle.store.getState()}
-          contract="Pedro_ERC20Token"
-          method="symbol"
-        />
+            contract="Pedro_ERC20Token"
+            method="symbol"
+          />
+        </div>
       </div>
-    </div>
     )
   }
 }
