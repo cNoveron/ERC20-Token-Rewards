ee1038f53f33625b800a2634f47cbf3d0198644c
carlosescom <carlos.escom.ipn@gmail.com>
2019-01-04 03:43:02 -0600

    Restructured component structure to follow React Context API v16.3+

--- src/index.js
+++ src/index.js
@@ -1,23 +1,7 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { Router, Route } from 'react-router'
-import { DrizzleProvider } from 'drizzle-react'
+import React from 'react'
+import ReactDOM from 'react-dom'
 
 // Layouts
 import App from './App'
-import { LoadingContainer } from 'drizzle-react-components'
 
-import { history, store } from './store'
-import drizzleOptions from './drizzleOptions'
-
-ReactDOM.render((
-    <DrizzleProvider options={drizzleOptions} store={store}>
-      <LoadingContainer>
-        <Router history={history} store={store}>
-          <Route exact path="/" component={App} />
-        </Router>
-      </LoadingContainer>
-    </DrizzleProvider>
-  ),
-  document.getElementById('root')
-);
+ReactDOM.render(<App/>, document.getElementById('root'));
