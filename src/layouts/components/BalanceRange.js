0e9209f3ae4f67997e4b7cf593154c46e91cb566
carlosescom <carlos.escom.ipn@gmail.com>
2019-01-06 22:16:23 -0600

    Implemented state updates based on received props

--- src/layouts/components/BalanceRange.js
+++ src/layouts/components/BalanceRange.js
@@ -1,23 +1,43 @@
 import React, { Component } from 'react'
+import { DrizzleContext } from 'drizzle-react'
+
+// Components
 import Balance from './Balance.js'
 import _ from 'lodash'
 
 class BalanceRange extends Component {
+  
+  constructor(props) {
+    super(props)
+    this.state = props.state
+  }
+
+  componentDidUdpate(prevProps) {
+    if (this.props !== prevProps)
+      this.setState(this.props.state)
+  }
 
   render() {
-    return <div>{
-      _
-      .range(0,this.props.accountsToRetrieve)
-      .map(
-        indexNum =>(
-          <Balance
-            drizzle={this.props.drizzle}
-            drizzleState={this.props.drizzle.store.getState()}
-            key={indexNum}
-            currentAccount={this.props.passedAccounts[indexNum]} />
-        )
+
+    if (_.isEmpty(this.state.drizzleState.accounts))
+      return (<div>503 - Service unavailable - BalanceRange:34 !this.props.drizzle.store.getState().accounts </div>)
+    else
+      return (
+        <div>{
+          _
+          .range(0,this.state.accountsToRetrieve)
+          .map(
+            index => (
+              <Balance
+                drizzle={this.state.drizzle}
+                drizzleState={this.state.drizzleState}
+                index={index}
+                key={index}
+                state={this.state} />
+            )
+          )
+        }</div>
       )
-    }</div>
   }
 }
 
