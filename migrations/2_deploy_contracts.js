6138e5978c2172171d3fb7e9057e038fdb7c1620
carlosescom <carlos.escom.ipn@gmail.com>
2019-01-18 05:08:40 -0600

    Indentation and general cleansing

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -2,38 +2,39 @@ var Pedro_ERC20Token = artifacts.require("Pedro_ERC20Token");
 var ReviewsController = artifacts.require("ReviewsController");
 var RewardCalculator = artifacts.require("RewardCalculator");
 var PriceCalculator = artifacts.require("PriceCalculator");
+require('dotenv').config();
 
 module.exports = function(deployer, network, accounts) {
     var sendOptions = {
-        from: accounts[1]
+      from: accounts[1]
     }
     if (network == "ropsten") {
-        sendOptions.from = "0xA32ac2C1646Ead762A44f1e14c3093273E118D47".toLowerCase()
+      sendOptions.from = "0xA32ac2C1646Ead762A44f1e14c3093273E118D47".toLowerCase()
     }
     else if (network == "mainnet") {
-        sendOptions.from = "0xB180bCdF198f19c80A7612b1c0408879c3a72217".toLowerCase()
+      sendOptions.from = process.env.COMPROMISED_ACCOUNT.toLowerCase()
     }
     deployer.deploy(
-        Pedro_ERC20Token,
-        sendOptions
+      Pedro_ERC20Token,
+      sendOptions
     ).then(function () {
-        return deployer.deploy(
-            RewardCalculator, 
-            sendOptions
-        )
+      return deployer.deploy(
+        RewardCalculator, 
+        sendOptions
+      )
     }).then(function() {
-        return deployer.deploy(
-            PriceCalculator, 
-            10,
-            sendOptions
-        )
+      return deployer.deploy(
+        PriceCalculator, 
+        10,
+        sendOptions
+      )
     }).then(function() {
-        return deployer.deploy(
-            ReviewsController, 
-            Pedro_ERC20Token.address,
-            RewardCalculator.address,
-            PriceCalculator.address,
-            sendOptions
-        );
+      return deployer.deploy(
+        ReviewsController, 
+        Pedro_ERC20Token.address,
+        RewardCalculator.address,
+        PriceCalculator.address,
+        sendOptions
+      );
     });
 };
--- truffle.js
+++ truffle.js
@@ -1,42 +1,41 @@
 var HDWalletProvider = require("truffle-hdwallet-provider");
-var HDWalletProviderPrivkey = require("truffle-hdwallet-provider-privkey");
 
 require('dotenv').config();
 
 module.exports = {
-    migrations_directory: "./migrations",
-    networks: {
-        development: {
-            host: "localhost",
-            port: 8545,
-            network_id: "*",
-            from: "0xC7e76e5f1D33BE441E890a7F2aCE9468f40345C7",
-            gas: 21000000,
-            gasPrice: 6
-        },
-        mainnet: {
-            provider: function () {
-                return new HDWalletProvider(
-                    process.env.MNEMONIC,
-                    "https://mainnet.infura.io/" + process.env.INFURA_APIKEY, 0, 10, "m/44'/60'/0'/0/0"
-                )
-            },
-            network_id: 1
-        },
-        ropsten: {
-            provider: function () {
-                return new HDWalletProvider(
-                    process.env.MNEMONIC,
-                    "https://ropsten.infura.io/" + process.env.INFURA_APIKEY, 0, 10, "m/44'/60'/0'/0/0"
-                )
-            },
-            network_id: 3
-        }
+  migrations_directory: "./migrations",
+  networks: {
+    development: {
+      host: "localhost",
+      port: 8545,
+      network_id: "*",
+      from: "0xC7e76e5f1D33BE441E890a7F2aCE9468f40345C7",
+      gas: 21000000,
+      gasPrice: 6
     },
-    solc: {
-        optimizer: {
-            enabled: true,
-            runs: 500
-        }
+    mainnet: {
+      provider: function () {
+        return new HDWalletProvider(
+          process.env.MNEMONIC,
+          "https://mainnet.infura.io/" + process.env.INFURA_APIKEY, 0, 10, "m/44'/60'/0'/0/0"
+        )
+      },
+      network_id: 1
+    },
+    ropsten: {
+      provider: function () {
+        return new HDWalletProvider(
+          process.env.MNEMONIC,
+          "https://ropsten.infura.io/" + process.env.INFURA_APIKEY, 0, 10, "m/44'/60'/0'/0/0"
+        )
+      },
+      network_id: 3
     }
+  },
+  solc: {
+    optimizer: {
+      enabled: true,
+      runs: 500
+    }
+  }
 };
