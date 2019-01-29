8909cedda4936b5093284febe6ed941690bd170c
carlosescom <carlos.escom.ipn@gmail.com>
2019-01-28 20:31:55 -0600

    Declared pay function parameters and return variable

--- contracts/RewardPayer.sol
+++ contracts/RewardPayer.sol
@@ -32,4 +32,25 @@ contract ReviewsController is IServiceStateController {
     pedro_ERC20Token = Pedro_ERC20Token(currentPedro_ERC20Token);
   }
 
+
+
+
+  function pay(
+    uint32 review_id,
+    uint32 provider_id,
+    bool service_done,
+    bool hire_again,
+    uint8 overall_rating,
+    uint service_price,
+    uint8 price_satisfaction,
+    uint8 responsiveness,
+    uint8 professionalism,
+    uint8 satisfaction
+  )
+  external
+  returns(
+    uint rewardAmount
+  )
+  {}
+
 }
