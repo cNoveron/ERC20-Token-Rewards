c00c58b4a4e1f36a28aad085ef6d4e82d495a3ec
carlosescom <carlos.escom.ipn@gmail.com>
2019-02-07 22:21:21 -0600

    Deprecated use of SafeERC20 due to unexpected behavior

--- contracts/RewardsPayer.sol
+++ contracts/RewardsPayer.sol
@@ -61,7 +61,6 @@ contract RewardsPayer {
 
 
   using SafeMath for uint256;
-  using SafeERC20 for Pedro_ERC20Token;
 
   function pay(
 /*     uint32 review_id,
@@ -78,11 +77,10 @@ contract RewardsPayer {
     address provider
   )
   public
-  payable
   {
     uint256 tokenAmount = _rewardAmount(service_price);
-    pedro_ERC20Token.safeTransfer(consumer, tokenAmount);
-    pedro_ERC20Token.safeTransfer(provider, tokenAmount);
+    pedro_ERC20Token.transfer(consumer, tokenAmount);
+    pedro_ERC20Token.transfer(provider, tokenAmount);
   }
 
   
@@ -132,48 +130,5 @@ contract RewardsPayer {
   {
     uint256 fractionOfPrice_inUSDCents = service_price.div(10);
     fractionOfPrice_inETHWei = USDCent_inETHWei.mul(fractionOfPrice_inUSDCents);
-  }
-}
-
-
-
-
-/**
- * @title SafeERC20
- * @dev Wrappers around ERC20 operations that throw on failure.
- * To use this library you can add a `using SafeERC20 for ERC20;` statement to your contract,
- * which allows you to call the safe operations as `token.safeTransfer(...)`, etc.
- */
-library SafeERC20 {
-  function safeTransfer(
-    Pedro_ERC20Token _token,
-    address _to,
-    uint256 _value
-  )
-    internal
-  {
-    require(_token.transfer(_to, _value),"Could not safely transfer from that address.");
-  }
-
-  function safeTransferFrom(
-    Pedro_ERC20Token _token,
-    address _from,
-    address _to,
-    uint256 _value
-  )
-    internal
-  {
-    require(_token.transferFrom(_from, _to, _value),"Could not safely transfer from that address.");
-  }
-
-  function safeApprove(
-    Pedro_ERC20Token _token,
-    address _spender,
-    uint256 _currentValue,
-    uint256 _value
-  )
-    internal
-  {
-    require(_token.approve(_spender, _currentValue, _value),"Could not safely approve, check current approved value.");
   }
 }
