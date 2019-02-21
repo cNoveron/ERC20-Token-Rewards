b304bf7e1e4f0c91da5ad1761d18a9c9ad2bad55
carlosescom <carlos.escom.ipn@gmail.com>
2019-02-20 20:51:48 -0600

    Deprecated truffle.js in favor of truffle-config.js

--- truffle-config.js
+++ truffle-config.js
@@ -1,10 +1,47 @@
+var HDWalletProvider = require("truffle-hdwallet-provider");
+
+require('dotenv').config();
+
 module.exports = {
   migrations_directory: "./migrations",
   networks: {
     development: {
       host: "localhost",
-      port: 8545,
-      network_id: "*" // Match any network id
+      port: 7545,
+      network_id: "*",
+      from: "0xC7e76e5f1D33BE441E890a7F2aCE9468f40345C7"
+    },
+    mainnet: {
+      provider: function () {
+        return new HDWalletProvider(
+          process.env.MNEMONIC,
+          "https://mainnet.infura.io/v3/" + process.env.INFURA_APIKEY, 0, 10, "m/44'/60'/0'/0/0"
+        )
+      },
+      network_id: 1
+    },
+    ropsten: {
+      provider: function () {
+        return new HDWalletProvider(
+          process.env.MNEMONIC,
+          "https://ropsten.infura.io/v3/" + process.env.INFURA_APIKEY, 0, 10, "m/44'/60'/0'/0/0"
+        )
+      },
+      network_id: 3
+    },
+    rinkeby: {
+      provider: function () {
+        return new HDWalletProvider(
+          process.env.MNEMONIC,
+          "https://rinkeby.infura.io/v3/" + process.env.INFURA_APIKEY, 0, 10, "m/44'/60'/0'/0/0"
+        )
+      },
+      network_id: 4
+    }
+  },
+  compilers: {
+    solc: {
+      version: "^0.4.15"
     }
   }
 };
