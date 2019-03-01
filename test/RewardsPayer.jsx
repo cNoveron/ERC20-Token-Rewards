b52b925fdb744e10aed270c549103a278e33716e
carlosescom <carlos.escom.ipn@gmail.com>
2019-03-01 01:18:43 -0600

    Added test for currentToken public variable

--- test/RewardsPayer.jsx
+++ test/RewardsPayer.jsx
@@ -10,6 +10,15 @@ contract("RewardsPayer", async accounts => {
     await RewardsPayer.deployed();
   }
 
+  it("RewardsPayer.currentToken() should return the address of Pedro_ERC20Token in the network.", async () => {
+    await deployEverything();
+    assert.equal(
+      await (await RewardsPayer.deployed()).currentToken.call(),
+      (await Pedro_ERC20Token.deployed()).address,
+      "Pedro_ERC20Token addresses are not equal."
+    );
+  });
+
   it("RewardsPayer.currentFiatContract() should return the address of FiatContract in the network.", async () => {
     await deployEverything();
     assert.equal(
