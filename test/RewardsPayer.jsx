95fc1219af1b0c28d2da7b6b24f3c044b376cd25
carlosescom <carlos.escom.ipn@gmail.com>
2019-03-01 22:46:19 -0600

    Removed unnecessary use of variable names

--- test/RewardsPayer.jsx
+++ test/RewardsPayer.jsx
@@ -1,30 +1,32 @@
-var Pedro_ERC20Token = artifacts.require("Pedro_ERC20Token");
-var FiatContract = artifacts.require("FiatContract");
-var RewardsPayer = artifacts.require("RewardsPayer");
+const deployEverything = async () => [
+  await artifacts.require("Pedro_ERC20Token").deployed(),
+  await artifacts.require("FiatContract").deployed(),
+  await artifacts.require("RewardsPayer").deployed()
+]
 
 contract("RewardsPayer", async accounts => {
 
-  async function deployEverything() {
-    await Pedro_ERC20Token.deployed();
-    await FiatContract.deployed();
-    await RewardsPayer.deployed();
-  }
-
   it("RewardsPayer.currentToken() should return the address of Pedro_ERC20Token in the network.", async () => {
-    await deployEverything();
+
+    let [Pedro_ERC20Token, FiatContract, RewardsPayer] = await deployEverything()
+
     assert.equal(
-      await (await RewardsPayer.deployed()).currentToken.call(),
-      (await Pedro_ERC20Token.deployed()).address,
+      await RewardsPayer.currentToken.call(),
+      Pedro_ERC20Token.address,
       "Pedro_ERC20Token addresses are not equal."
     );
   });
 
   it("RewardsPayer.currentFiatContract() should return the address of FiatContract in the network.", async () => {
-    await deployEverything();
+
+    let [Pedro_ERC20Token, FiatContract, RewardsPayer] = await deployEverything()
+
     assert.equal(
-      await (await RewardsPayer.deployed()).currentFiatContract.call(),
-      (await FiatContract.deployed()).address,
+      await RewardsPayer.currentFiatContract.call(),
+      FiatContract.address,
       "FiatContract addresses are not equal."
     );
+
   });
+
 });
